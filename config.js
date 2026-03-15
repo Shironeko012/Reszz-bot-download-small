de81fc96ae6f2a2889e62796c9d5672c8531a111
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-15 10:01:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 require("dotenv").config()
 
 module.exports = {
+
  prefix: process.env.PREFIX || ".",
+
  owner: process.env.OWNER || "",
- maxSizeMB: Number(process.env.MAX_SIZE_MB) || 50,
+
+ maxFileSizeMB: Number(process.env.MAX_FILE_SIZE_MB) || 50,
+
  rateLimit: Number(process.env.RATE_LIMIT) || 10,
- tempDir: "./temp",
+
+ downloadTimeout: 600000,
+
+ tempDir: "./temp"
+
 }
