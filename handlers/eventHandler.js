88fa9ebddf2cdeef2e2506e8c3518160c8d8c4fb
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-15 07:44:42 +0700

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -6,15 +6,22 @@ module.exports = (sock) => {
 
   try {
 
-   const msg = messages?.[0]
+   if (!messages) return
+   if (!Array.isArray(messages)) return
+
+   const msg = messages[0]
 
    if (!msg) return
-
    if (!msg.message) return
 
+   // ignore broadcast
    if (msg.key?.remoteJid === "status@broadcast") return
 
+   // ignore system messages
    if (msg.message.protocolMessage) return
+   if (msg.message.reactionMessage) return
+   if (msg.message.senderKeyDistributionMessage) return
+   if (msg.message.messageContextInfo) return
 
    await messageHandler(sock, msg)
 
